0e291594fea0fcf0aefd73f8b522dc5332338621
Clint Worrell <github@clintworrell.com>
2016-04-19 15:41:32 -0700

    Calculate volumes of various shapes given certain conditions.

---
+++ volumes.js
@@ -0,0 +1,15 @@
+var pi = Math.PI;
+
+var sphereRadius = 15;
+
+var coneBase = 11;
+var coneHeight = 22;
+
+var rectLength = 5;
+var rectHeight = 11;
+var rectWidth = 467;
+
+
+var sphereVolume = (4 / 3) * pi * Math.pow(sphereRadius, 3);
+var coneVolume = (1 / 3) * coneBase * coneHeight;
+var rectPrismVolume = rectLength * rectHeight * rectWidth;
